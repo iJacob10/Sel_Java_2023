307f6a709439f0ecafadcf75e85c17d70ae8296f
iJacob10 <06iylinjacob@gmail.com>
2023-08-08 22:56:06 +0530

    locators
    Co-Authored-By: Iylin princy <99063224+iylin@users.noreply.github.com>

--- Sel_Java23/src/_03_Locators.java
+++ Sel_Java23/src/_03_Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -7,13 +9,14 @@ public class _03_Locators {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D:/Sel_Java_2023/chromedriver.exe");
+		// Implicit Wait
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
 		driver.findElement(By.id("APjFqb")).sendKeys("Twitter");
 		driver.findElement(By.xpath("//*[@id=\'APjFqb\']")).sendKeys(Keys.ENTER);
-		Thread.sleep(2000);
 		// driver.findElement(By.xpath("//input[@class='required_field
 		// cityPadRight']")).sendKeys("DELHI");
 		// driver.findElement(By.cssSelector("input[class='required_field
@@ -25,10 +28,8 @@ public class _03_Locators {
 		 * Class Name -> tagname.classname ID -> tagname#id Tagname[attribute='value']
 		 */
 		// Scroll Down
-
 		Thread.sleep(5000);
 		driver.findElement(By.cssSelector("input[autocapitalize='sentences']")).sendKeys("Testing");
-		Thread.sleep(2000);
 		// Click NEXT
 		driver.findElement(By.xpath("(//span[@class=\'css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0\'])[7]"))
 				.click();
